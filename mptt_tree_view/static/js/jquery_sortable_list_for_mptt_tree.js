b77867fea5bc2ebab17b11e8d00fcf6ed2b7400f
Richard <richardwangwang@gmail.com>
2016-04-11 00:06:05 +0800

    Use new li's pk instead of ul because newly create ul has no pk.

--- mptt_tree_view/static/js/jquery_sortable_list_for_mptt_tree.js
+++ mptt_tree_view/static/js/jquery_sortable_list_for_mptt_tree.js
@@ -3,7 +3,7 @@ var options = {
 
             console.log(currEl);
             var curLi = currEl;//parseInt($(currEl.parents("li")[0])
-            var parentPk = $(curLi.parents("ul")[0]).attr("pk");
+            var parentPk = $(curLi.parents("li")[0]).attr("pk");
             if(parentPk=="-1") parentPk="None";
             if(parentPk!=curLi.attr("parent")){
                 $("#item-list").mpttAjax('updateParent', currEl.attr("pk"), parentPk);
